d1d411777f31d0dfefd3cb2ba72cb716f976c90a
Bojan <bojan.kovacec@loewe.de>
2016-10-30 19:10:33 +0100

    Local Theme

---
+++ Themes/Local/gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require("gulp");
+var shell = require('gulp-shell');
+var elixir = require('laravel-elixir');
+var themeInfo = require('./theme.json');
+
+
+//elixir.extend("stylistPublish", function() {
+//    gulp.task("stylistPublish", function() {
+//        return gulp.src("").pipe(shell("php ../../artisan stylist:publish "+themeInfo.name));
+//    }).watch("**/*.less");
+//});
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function (mix) {
+
+    /**
+     * Compile less
+     */
+    mix.less([
+        "main.less"
+    ], '../../public/themes/' + themeInfo.name.toLowerCase() + '/css');
+
+    // .stylistPublish();
+
+    /**
+     * Concat scripts
+     */
+    mix.scripts([
+        '/vendor/jquery/dist/jquery.js',
+        '/vendor/bootstrap/dist/js/bootstrap.min.js',
+        '/vendor/prism/prism.js',
+        '/js/bootswatch.js'
+    ], null, 'resources');
+
+    /**
+     * Copy Bootstrap fonts
+     */
+    mix.copy(
+        'resources/vendor/bootstrap/fonts',
+        'assets/fonts'
+    );
+
+    /**
+     * Copy Fontawesome fonts
+     */
+    mix.copy(
+        'resources/vendor/font-awesome/fonts',
+        'assets/fonts'
+    );
+
+    /**
+     * Copy assets to public
+     */
+    mix.copy(
+        'assets',
+        '../../public/themes/' + themeInfo.name.toLowerCase()
+    )
+
+
+    /**
+     * Copy css to public
+
+    mix.copy(
+        'public/css',
+        '../../public/themes/' + themeInfo.name.toLowerCase() + '/css'
+    )
+    */
+
+
+    /**
+     * Copy js to public
+
+    mix.copy(
+        'public/js',
+        '../../public/themes/' + themeInfo.name.toLowerCase() + '/js'
+    )
+     */
+
+});
---
+++ Themes/Local/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head lang="{{ LaravelLocalization::setLocale() }}">
+    <meta charset="UTF-8">
+    @section('meta')
+        <meta name="description" content="{{ Setting::get('core::site-description') }}" />
+    @show
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        @section('title'){{ Setting::get('core::site-name') }}@show
+    </title>
+    <link rel="shortcut icon" href="{{ Theme::url('favicon.ico') }}">
+
+    {!! Theme::style('css/app.css') !!}
+</head>
+<body>
+
+@include('partials.navigation')
+
+<div class="container">
+
+    <h1>TEST</h1>
+    @yield('content')
+</div>
+@include('partials.footer')
+
+{!! Theme::script('js/all.js') !!}
+@yield('scripts')
+
+<?php if (Setting::has('core::google-analytics')): ?>
+{!! Setting::get('core::google-analytics') !!}
+<?php endif; ?>
+</body>
+</html>
